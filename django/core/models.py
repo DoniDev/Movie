4f2c81c5587b0c19bce846b247186220c5b6e987
DoniDev <abduvokhidovdoniyor4@gmail.com>
2020-07-25 01:54:51 +0500

    user app added

--- django/core/models.py
+++ django/core/models.py
@@ -20,6 +20,7 @@ class Person(models.Model):
     died = models.DateField(null=True, blank=True)
 
     objects = PersonManager()
+    # objects = Manager()
 
     class Meta:
         ordering = ('last_name', 'first_name')
@@ -30,6 +31,9 @@ class Person(models.Model):
 
         return '{} {} {}'.format(self.last_name, self.first_name, self.born)
 
+    def get_absolute_url(self):
+        return reverse('core:PersonDetail', kwargs={'pk': self.id})
+
 
 class Role(models.Model):
     movie = models.ForeignKey(to='Movie', on_delete=models.DO_NOTHING)
@@ -37,7 +41,7 @@ class Role(models.Model):
     name = models.CharField(max_length=150)
 
     def __str__(self):
-        return '{} {] {}'.format(self.movie.id, self.person.id, self.name)
+        return '{} {} {}'.format(self.movie.id, self.person.id, self.name)
 
     class Meta:
         unique_together = (
--- django/core/urls.py
+++ django/core/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from . views import (
     MovieList,
     MovieDetail,
+    PersonDetail,
 )
 
 app_name = 'core'
 urlpatterns = [
     path('movies/',MovieList.as_view(), name='MovieList'),
     path('movie/<int:pk>/', MovieDetail.as_view(), name='MovieDetail'),
+    path('person/<int:pk>/', PersonDetail.as_view(), name='PersonDetail'),
 
 ]
